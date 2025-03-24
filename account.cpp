c6fc40550dd87c5759cab32dc0a4fac94f72bcab
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-03-24 15:10:15 -0500

    comments from account.cpp

--- account.cpp
+++ account.cpp
@@ -2,26 +2,20 @@
 #include <iostream>
 #include <iomanip>
 
-// Initialize static variable
 int Account::nextAccountNumber = 1000;
 
-// Default Constructor
 Account::Account() : accountNumber(new int(nextAccountNumber++)), customerName(""), balance(0.0) {}
 
-// Parameterized Constructor
 Account::Account(const std::string& name, double initialDeposit)
     : accountNumber(new int(nextAccountNumber++)), customerName(name), balance(initialDeposit) {}
 
-// Destructor
 Account::~Account() {
     delete accountNumber;
 }
 
-// Copy Constructor
 Account::Account(const Account& other)
     : accountNumber(new int(*other.accountNumber)), customerName(other.customerName), balance(other.balance) {}
 
-// Copy Assignment Operator
 Account& Account::operator=(const Account& other) {
     if (this != &other) {
         *accountNumber = *other.accountNumber;
@@ -31,7 +25,6 @@ Account& Account::operator=(const Account& other) {
     return *this;
 }
 
-// Accessors
 int Account::getAccountNumber() const {
     return *accountNumber;
 }
@@ -44,7 +37,6 @@ double Account::getBalance() const {
     return balance;
 }
 
-// Mutators
 void Account::setCustomerName(const std::string& name) {
     customerName = name;
 }
@@ -61,12 +53,10 @@ void Account::withdraw(double amount) {
     }
 }
 
-// Overloaded Operator
 bool Account::operator==(const Account& other) const {
     return *accountNumber == *other.accountNumber;
 }
 
-// Static function to get the next account number
 int Account::getNextAccountNumber() {
     return nextAccountNumber;
 }
